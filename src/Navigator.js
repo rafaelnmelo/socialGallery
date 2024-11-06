96d95af7d885d7197cd3b7b92c429be7122a2550
Rafael Nunes <rafaelnunesmelo@gmail.com>
2024-11-06 11:16:57 -0300

    Iniciando tela de registro de usuário

--- src/Navigator.js
+++ src/Navigator.js
@@ -8,7 +8,7 @@ import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
 import Login from './screens/Login'
-// import Register from './screens/Register'
+import Register from './screens/Register'
 
 // import useUser from './data/hooks/useUser'
 
@@ -28,7 +28,7 @@ export default props => {
     const Auth = () => (
         <AuthStack.Navigator initialRouteName="Login">
             <AuthStack.Screen name="Login" component={Login} />
-            <AuthStack.Screen name="Register" component={Feed} />
+            <AuthStack.Screen name="Register" component={Register} />
         </AuthStack.Navigator>
     )
 
